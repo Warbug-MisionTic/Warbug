2191829e75604a13b078530fa1ec50fbe8a9c813
Sebastián <sebaastian0612@hotmail.com>
2022-09-05 18:27:32 -0500

    Se modifican los errores en la clase Profile

--- src/main/java/com/misiontic/warbug/repository/IProfileRepository.java
+++ src/main/java/com/misiontic/warbug/repository/IProfileRepository.java
@@ -1,6 +1,7 @@
 package com.misiontic.warbug.repository;
 
-import org.springframework.context.annotation.Profile;
+
+import com.misiontic.warbug.models.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
