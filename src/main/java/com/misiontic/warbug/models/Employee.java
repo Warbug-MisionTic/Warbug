afcd3954dd1ab540c3aa5e1f8f277b28efac2024
Mauricio Alvarez Heredia <mauro.h01@gmail.com>
2022-09-16 15:00:37 -0500

    Se corrigieron las rutas de los css

--- src/main/java/com/misiontic/warbug/models/Employee.java
+++ src/main/java/com/misiontic/warbug/models/Employee.java
@@ -32,6 +32,6 @@ public class Employee {
     @JoinColumn(name = "fk_enterprise_id",nullable = false)
     private Enterprise enterprise;
 
-    @Column(length = 50, nullable = fals    e)
+    @Column(length = 50, nullable = false)
     private String password;
 }
